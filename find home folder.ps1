c07cad9a0dfa8f31e961e3d24efadff33d0b9167
1michalS <40490333+1michalS@users.noreply.github.com>
2018-12-03 11:32:59 +0100

    Update find home folder.ps1

--- find home folder.ps1
+++ find home folder.ps1
@@ -1,8 +1,1 @@
-﻿$user = '-to_be_deleted_JUUSEMA1'
-
-(Get-ChildItem \\ocnet.local\home\*\*\  -filter $user | ?{$_.Name -match [regex]::Escape($User)}).FullName
-
-
-
-
-#(Get-ChildItem \\ocnet.local\home\SE002\).name 
+(Get-ChildItem \\domain\home\*\*\  -filter $user | ?{$_.Name -match [regex]::Escape($User)}).FullName
